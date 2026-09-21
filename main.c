90dfbc8125d412112f9c95aaad026fb4b692dae2
gianna810 <gianna0810@naver.com>
2026-09-21 19:53:42 +0900

    실습4

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-    char c;
+    float a, b;
 
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("분자를 입력하세요 : ");
+    scanf("%f", &a);
 
-    c = c + 1;
+    printf("분모를 입력하세요 : ");
+    scanf("%f", &b);
 
-    printf("the next character of %c (%i) is %c (%i)\n",
-           c - 1, c - 1, c, c);
+    printf("나누기의 결과는 %f 입니다.\n", a / b);
 
     return 0;
 }
